d7d03b2b269136a394117460285dd5303c2de974
Dani PC <Dani PC@192.168.0.18>
2021-05-25 23:56:31 -0500

    Cargando formularios y primer intento para conectar a SQL server.

---
+++ src/main/java/Conexion/Conexion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Dani PC
+ */
+public class Conexion {
+    
+    public static Connection getConexion(){
+        
+        String url = "jdbc:sqlserver://DESKTOP-1MCDJFH:1433;" 
+                + "database=Punto_Venta;"
+                + "user=sa;"
+                + "password=dani2000;";
+        
+        try{
+            
+            Connection con = DriverManager.getConnection(url);
+            return con;
+        
+        }catch(SQLException e){
+        
+            System.out.println(e.toString());
+            return null;
+        }
+    
+    }
+    
+}
